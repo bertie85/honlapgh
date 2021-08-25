b22eaad89d77ff1f8a57309a03c03f2fba2ea6e4
Bertold Szekeres <bertold.szekeres@gmail.com>
2021-08-25 14:16:06 +0200

    a few small things

--- src/pages/dokumentumok/index.js
+++ src/pages/dokumentumok/index.js
@@ -1,8 +1,13 @@
 import Page from '../../components/page/page';
+import Section from '../../components/section/section';
 
 const Dokumentumok = (props) => (
   <Page title="Dokumentumok">
-    
+    <Section title="Étkeztetés">
+
+      <a href="https://drive.google.com/uc?export=download&id=1j1uaG6QQz5A5BnerwLM_DKVMCPHG0VJN" download={true}>download</a>
+    </Section>
+
   </Page>
 );
 
